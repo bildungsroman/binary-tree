63440173f6e6bfde676d0dd007422092846c0207
bildungsroman <anna.spysz@stackery.io>
2021-06-23 15:45:32 -0700

    Decided to torture myself for fun

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const printBinaryTree = () => {
-  console.log(`          1          `);
-  console.log(`         010         `);
-  console.log(`        10101        `);
-  console.log(`       0101010       `);
-  console.log(`      101010101      `);
-  console.log(`     01010101010     `);
-  console.log(`    1010101010101    `);
-  console.log(`      010101010      `);
-  console.log(`   101010101010101   `);
-  console.log(`      010101010      `);
-  console.log(`  10101010101010101  `);
-  console.log(`         000         `);
-  console.log(`         000         `);
-  console.log(`         111         `);
+  for (let i = 1; i < 14; i++) {
+    const spaces = 21 - i;
+    let statement = ``;
+    for (let j = 1; j < spaces; j++) {
+      statement += ` `;
+    }
+    if (i === 1) {
+      statement += `1`;
+    } else {
+      i % 2 == 0 ? statement += `01`.repeat(i-1) + `0` : statement += `10`.repeat(i-1) + `1`;
+    }
+    console.log(statement);
+  }
+  for (let i = 1; i < 4; i++) {
+    i !== 3 ? console.log(`                  000`) : console.log(`                  111`);
+  }
 }
 
 module.exports = { printBinaryTree };
