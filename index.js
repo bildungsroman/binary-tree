ae4916018630948e35a58485e15b6348bfa20c86
bildungsroman <anna.spysz@stackery.io>
2021-06-23 14:53:13 -0700

    Added function code and packages

---
+++ index.js
@@ -0,0 +1,18 @@
+const printBinaryTree = () => {
+  console.log(`          1          `);
+  console.log(`         010         `);
+  console.log(`        10101        `);
+  console.log(`       0101010       `);
+  console.log(`      101010101      `);
+  console.log(`     01010101010     `);
+  console.log(`    1010101010101    `);
+  console.log(`      010101010      `);
+  console.log(`   101010101010101   `);
+  console.log(`      010101010      `);
+  console.log(`  10101010101010101  `);
+  console.log(`         000         `);
+  console.log(`         000         `);
+  console.log(`         111         `);
+}
+
+module.exports = { printBinaryTree };
